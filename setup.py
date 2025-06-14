2f9d8d52cac8218c8a748bbe529a239c7dbcce19
cms66 <58144229+cms66@users.noreply.github.com>
2025-06-14 19:34:06 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,1 +1,27 @@
-# Base setup
+# First boot - Base setup
+# Imports
+import  subprocess, sys, os, urllib.request
+
+def main():
+    print("Setup")
+    bashfile = "base_setup.sh"
+    bashurl = "https://raw.githubusercontent.com/cms66/rpi-pycluster/main/" + bashfile
+    try:
+        urllib.request.urlretrieve(bashurl, bashfile) # Download bash script
+        subprocess.run(["sudo", "bash", "./" + bashfile], check=True) # Sudo run bash script
+        subprocess.run(["sudo", "rm", "-f", "./" + bashfile], check=True) # Delete bash script
+        usropt=input("Base seup done, press p to poweroff or any other key to reboot: ").lower()
+        os.remove(__file__) # Delete python script
+        if usropt == 'p':
+            print ("Poweroff selected")
+            subprocess.call(["shutdown", "-s", "now"])
+        else:
+            print ("Reboot selected")
+            subprocess.call(["shutdown", "-r", "now"])
+    except Exception as e:
+        print("Error:", e)
+        sys.exit(1)
+    print("Python setup done")
+    
+if __name__ == "__main__":
+    main()
