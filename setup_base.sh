51f247c3326e6246cb3337cbd3f4fd5fc80e28d5
cms66 <58144229+cms66@users.noreply.github.com>
2025-06-17 14:14:44 +0100

    Update setup_base.sh

--- setup_base.sh
+++ setup_base.sh
@@ -1,9 +1,9 @@
 rm -rf /usr/local/sdm
 rm -rf /usr/local/bin/sdm
 rm -rf /etc/sdm
-usrname=$(logname) # TODO
-echo "alias spo=\"sudo poweroff\"" >> /home/$usrname/.bashrc # TODO
-echo "alias spr=\"sudo reboot\"" >> /home/$usrname/.bashrc # TODO 
+usrname=$(logname)
+echo "alias spo=\"sudo poweroff\"" >> /home/$usrname/.bashrc
+echo "alias spr=\"sudo reboot\"" >> /home/$usrname/.bashrc 
 sed -i "s/#FallbackNTP/FallbackNTP/g" /etc/systemd/timesyncd.conf # Setup NTP
 #Pi 5 + CM5 config.txt
 echo "dtparam=pciex1_gen=3" >> /boot/firmware/config.txt
